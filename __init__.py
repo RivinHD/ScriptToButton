80f90726d16bc565296124b298b57598baa40b38
RivinHD <58261670+RivinHD@users.noreply.github.com>
2020-06-21 15:22:18 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,186 @@
+import bpy
+from bpy.props import StringProperty, EnumProperty, PointerProperty, CollectionProperty, BoolProperty
+import os
+
+bl_info = {
+    "name" : "Scripts To Button",
+    "author" : "RivinHD",
+    "blender" : (2, 83, 0),
+    "version" : (1, 0, 0),
+    "location" : "View3D",
+    "category" : "Generic"
+}
+
+PanelInvoke = [True]
+
+def SaveText(ActiveText, ScriptName):
+    #In real add-on: os.path.dirname(__file__) + "\Storage\mytxt.py"
+    text = ActiveText.as_string()
+    destination = os.path.dirname(__file__) + "/Storage/" + ScriptName + ".py"
+    with open(destination, 'w', encoding='utf8') as outfile:
+        outfile.write(text)
+
+def GetText(ScriptName):
+    destination = os.path.dirname(__file__) + "/Storage/" + ScriptName + ".py"
+    bpy.data.texts.new(ScriptName)
+    with open(destination, 'r', encoding='utf8') as infile:
+        bpy.data.texts[ScriptName].write(infile.read())
+
+def GetAllSavedScripts():
+    l = []
+    for file in os.listdir(os.path.dirname(__file__) + "/Storage"):
+        l.append(file.replace(".py",""))
+    return l
+
+
+classes = []
+class AddButtonPanel(bpy.types.Panel):
+    bl_idname = "stb.addpanel"
+    bl_label = "Add Button"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Scripts To Button"
+
+    def draw(self, context):
+        stb = context.scene.p_stb
+        layout = self.layout
+        col = layout.column()
+        col.prop(stb, "ButtonName")
+        col.prop(stb, "TextsList")
+        col.operator(AddButton.bl_idname)
+classes.append(AddButtonPanel) 
+
+class AddButton(bpy.types.Operator):
+    bl_idname = "stb.addbutton"
+    bl_label = "Add Button"
+    bl_description = "button"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        stb = context.scene.p_stb
+        if stb.ButtonName == '':
+            self.report({'ERROR'}, "You need a name for the Button")
+        elif stb.ButtonName in GetAllSavedScripts():
+            self.report({'ERROR'}, "You need an other Button name, this name is already used")
+        elif stb.TextsList == '':
+            self.report({'ERROR'}, "You need to select a Text")
+        else:
+            SaveText(bpy.data.texts[stb.TextsList], stb.ButtonName)
+            new = context.scene.c_stb.add()
+            new.name = stb.ButtonName
+            new.btn_name = stb.ButtonName
+            new = context.scene.t_stb.add()
+            new.name = stb.ButtonName
+            new.btn_name = stb.ButtonName
+            GetText(stb.ButtonName)
+        return {"FINISHED"}
+
+classes.append(AddButton)
+
+def textlist(self, context):
+    l = []
+    for i in bpy.data.texts:
+        st1 = i.name
+        st2 = i.name
+        st3 = ""
+        l.append((st1,st2,st3))
+    return l
+
+class AddPropertys(bpy.types.PropertyGroup):
+    ButtonName : StringProperty(name = "Button Name", description = "Set the name of the Button", default = "")
+    TextsList : EnumProperty(name="Text", description = "Chose Text for converting into Bar", items = textlist)
+classes.append(AddPropertys)
+
+class ButtonPanle(bpy.types.Panel):
+    bl_idname = "stb.panel"
+    bl_label = "Buttons"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Scripts To Button"
+
+    def draw(self, context):
+        layout = self.layout
+        if PanelInvoke[0]:
+            layout.operator(LoadButtons.bl_idname)
+        for i in range(len(context.scene.c_stb)):
+            row = layout.row().split(factor = 0.9)
+            btn = context.scene.c_stb[i]
+            tbtn = context.scene.t_stb[i]
+            row.operator(ScriptButton.bl_idname, text = btn.name).btn_name = btn.name
+            row.operator(TrashButton.bl_idname, text = '', icon = 'TRASH').trash_name = tbtn.name
+
+classes.append(ButtonPanle)
+
+class ScriptButton(bpy.types.Operator):
+    bl_idname = "stb.scriptbutton"
+    bl_label = "ScriptButton"
+    bl_options = {"REGISTER","UNDO"}
+
+    btn_name : StringProperty()
+
+    def execute(self, context):
+        text = bpy.data.texts[self.btn_name]
+        ctx = bpy.context.copy()
+        ctx['edit_text'] = text
+        bpy.ops.text.run_script(ctx)
+        return {"FINISHED"}
+classes.append(ScriptButton)
+
+class LoadButtons(bpy.types.Operator):
+    bl_idname = "stb.loadbuttons"
+    bl_label = "Load"
+    bl_options = {"REGISTER"}
+
+    def execute(self, context):
+        if PanelInvoke[0]: #initalize buttons for first start
+            PanelInvoke[0] = False
+            for script in GetAllSavedScripts():
+                new = context.scene.c_stb.add()
+                new.name = script
+                new.btn_name = script
+                new = context.scene.t_stb.add()
+                new.name = script
+                new.btn_name = script
+                GetText(script)
+            PanelInvoke[0] = False
+        return {"FINISHED"}
+classes.append(LoadButtons)
+
+class ButtonPropertys(bpy.types.PropertyGroup):
+    btn_name = StringProperty()
+classes.append(ButtonPropertys)
+
+class TrashButton(bpy.types.Operator):
+    bl_idname = "stb.trashbutton"
+    bl_label = ""
+    bl_description = "Delet the Button next to it"
+    bl_options = {"REGISTER"}
+
+    trash_name = StringProperty()
+
+    def execute(self, context):
+        index = int(context.scene.t_stb[self.trash_name].path_from_id().replace("t_stb[","").replace("]",""))
+        os.remove(os.path.dirname(__file__) + "/Storage/" + context.scene.c_stb[index].name + ".py")
+        bpy.data.texts.remove(bpy.data.texts[context.scene.c_stb[index].name])
+        context.scene.c_stb.remove(index)
+        context.scene.t_stb.remove(index)
+        return {"FINISHED"}
+classes.append(TrashButton)
+
+class TrashPropertys(bpy.types.PropertyGroup):
+    trash_name = StringProperty()
+classes.append(TrashPropertys)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.p_stb = PointerProperty(type = AddPropertys)
+    bpy.types.Scene.c_stb = CollectionProperty(type = ButtonPropertys)
+    bpy.types.Scene.t_stb = CollectionProperty(type = TrashPropertys)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.p_stb
+    del bpy.types.Scene.c_stb
+    del bpy.types.Scene.t_stb
