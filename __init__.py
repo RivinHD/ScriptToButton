7f990655d3f60db5750aaeb29083b8783be7d373
RivinHD <58261670+RivinHD@users.noreply.github.com>
2020-07-15 13:01:23 +0200

    Version Update

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name" : "Script To Button",
     "author" : "RivinHD",
     "blender" : (2, 83, 0),
-    "version" : (1, 0, 3),
+    "version" : (1, 0, 4),
     "location" : "View3D",
     "category" : "Generic"
 }
